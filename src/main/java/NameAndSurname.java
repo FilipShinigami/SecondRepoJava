0234547cb8be4ac4c746eccbae5b1b0c3be3618b
Filip Koceski <filip.koceski2001@gmail.com>
2024-03-04 10:25:45 +0100

    Add NameAndSurname.java

---
+++ src/main/java/NameAndSurname.java
@@ -0,0 +1,14 @@
+
+public class NameAndSurname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Name = "Filip";
+		String Surname = "Koceski";
+		
+		System.out.println(Name + " " +Surname);
+		
+	}
+
+}
